493ecc252e7d95c1246b1def653338ebb6a32c85
PC Simon <simtho0624@gmail.com>
2022-10-26 15:01:09 +0200

    view done

--- src/main/java/se/iths/labb3/HelloController.java
+++ src/main/java/se/iths/labb3/HelloController.java
@@ -1,14 +1,33 @@
 package se.iths.labb3;
 
 import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+    public ChoiceBox forTheShape;
+    public ChoiceBox forTheColor;
+    public ChoiceBox forTheSize;
+    public Button buttonToApply;
+    public Canvas shapeHome;
+    public Pane paneIsInTheBack;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+
+    public void initialize(){
+        paneIsInTheBack.setStyle("-fx-background-color: #C0C0C0;");
+
+
+
+    }
+
+
+    public void drawTheShape(MouseEvent mouseEvent) {
+
     }
 }
