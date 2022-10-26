c74f30aade43bdb63e5ba2a4cf723ca111a3b91d
PC Simon <simtho0624@gmail.com>
2022-10-26 15:11:21 +0200

    whay

--- src/main/java/se/iths/labb3/controller/HelloController.java
+++ src/main/java/se/iths/labb3/controller/HelloController.java
@@ -1,14 +1,10 @@
 package se.iths.labb3.controller;
 
-import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
 
 public class HelloController {
     public ChoiceBox forTheShape;
@@ -16,11 +12,11 @@ public class HelloController {
     public ChoiceBox forTheSize;
     public Button buttonToApply;
     public Canvas shapeHome;
-    public Pane paneIsInTheBack;
+    public Pane paneColor;
 
 
     public void initialize(){
-        paneIsInTheBack.setStyle("-fx-background-color: #C0C0C0;");
+        paneColor.setStyle("-fx-background-color: #C0C0C0;");
 
 
 
